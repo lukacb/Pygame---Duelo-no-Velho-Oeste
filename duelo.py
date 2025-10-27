7c61e307b118643a3fcdb0ab5b7077d7e22074dc
Luka <lukacb@al.insper.edu.br>
2025-10-27 18:38:38 -0300

    Tentando andar com o boneco

--- duelo.py
+++ duelo.py
@@ -6,8 +6,18 @@ pygame.init()
 largura_tela = 800
 altura_tela = 600
 
-tela = pygame.display.set_mode((largura_tela, altura_tela)) #superficie principal onde tudo será desenhado
+tela = pygame.display.set_mode((largura_tela, altura_tela)) #Superficie principal onde tudo será desenhado
 pygame.display.set_caption("Duelo no Velho-Oeste")
+
+PRETO = (0, 0, 0)
+BRANCO = (255, 255, 255)
+CINZA_FUNDO = (100, 100, 100) 
+
+jogador1_rect = pygame.Rect(350, 250, 50, 100) 
+jogador2_rect = pygame.Rect(400, 250, 50, 100)
+velocidade = 1
+
+clock = pygame.time.Clock() #Devo controlar a velocidade dos frames
 
 rodando = True
 while rodando:
@@ -15,10 +25,16 @@ while rodando:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             rodando = False
+    
+    jogador1_rect.x = jogador1_rect.x - velocidade
+    jogador2_rect.x = jogador2_rect.x + velocidade
+    tela.fill(CINZA_FUNDO)
+    pygame.draw.rect(tela, BRANCO, jogador1_rect)
+    pygame.draw.rect(tela, BRANCO, jogador2_rect)
+    pygame.display.flip() #Atualiza a tela
 
-    cor_fundo = (0, 0, 0)
-    tela.fill(cor_fundo)
-    pygame.display.flip()
+    clock.tick(60)
+
 
 pygame.quit()
 
