5708f54b027b6568a40789ac32c2035744ce8b5b
Luka <lukacb@al.insper.edu.br>
2025-10-28 10:17:13 -0300

    Tela verde

--- duelo.py
+++ duelo.py
@@ -11,12 +11,18 @@ pygame.display.set_caption("Duelo no Velho-Oeste")
 
 PRETO = (0, 0, 0)
 BRANCO = (255, 255, 255)
-CINZA_FUNDO = (100, 100, 100) 
+CINZA_FUNDO = (100, 100, 100)
+VERDE = (0,255,0)
 
 jogador1_rect = pygame.Rect(350, 250, 50, 100) 
 jogador2_rect = pygame.Rect(400, 250, 50, 100)
 velocidade = 1
 estado_jogo = "ANDANDO"
+tempo_sinal = random.uniform(3.0,5.0)
+print(f"Tempo sorteado: {tempo_sinal} segundos")
+
+tempo_inicio_espera = 0
+sinal_ativo = False 
 
 clock = pygame.time.Clock() #Devo controlar a velocidade dos frames
 
@@ -33,7 +39,21 @@ while rodando:
         if jogador1_rect.left < 100 or jogador2_rect.right > 700:
             print("Parados! Esperando o sinal...")
             estado_jogo = "ESPERANDO"
-    tela.fill(CINZA_FUNDO)
+            tempo_inicio_espera = pygame.time.get_ticks()
+    elif estado_jogo == "ESPERANDO":
+        tempo_agora = pygame.time.get_ticks()
+        tempo_passados_ms = tempo_agora - tempo_inicio_espera
+        tempo_sinal_ms = tempo_sinal * 1000
+        if tempo_passados_ms > tempo_sinal_ms:
+            print("SINAL VERDE!")
+            estado_jogo = "SINAL"
+            sinal_ativo = True
+    
+    if sinal_ativo == True:
+        tela.fill(VERDE)
+    else:
+        tela.fill(CINZA_FUNDO)
+
     pygame.draw.rect(tela, BRANCO, jogador1_rect)
     pygame.draw.rect(tela, BRANCO, jogador2_rect)
     pygame.display.flip() #Atualiza a tela
